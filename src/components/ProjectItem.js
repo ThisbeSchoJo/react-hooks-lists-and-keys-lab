fee7978ce7f0374533d0b48336821c147d2a44f4
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-01-28 14:41:55 -0500

    complete lab

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,15 +1,24 @@
 import React from "react";
+// import ProjectList from "./ProjectList";
 
 function ProjectItem({ name, about, technologies }) {
+  console.log(technologies)
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+      <div className="technologies">{technologies.map((technology, index) => (
+        <span key={index}>{technology}</span>
+      ))}
       </div>
     </div>
   );
 }
 
+
 export default ProjectItem;
+
+// 0: "React"
+// 1: "Redux"
+// 2: "Rails"
+
